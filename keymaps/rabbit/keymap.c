898f94afacbbf5120b0c0074069f2ac9ca0b5d05
Joey Wu <yoj845@gmail.com>
2023-02-13 22:33:54 +0800

    💄 update keymap

--- keymaps/rabbit/keymap.c
+++ keymaps/rabbit/keymap.c
@@ -34,12 +34,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BS] = LAYOUT(
-        CPY_PST, ALT_TAB, KC_AT  , KC_HASH, KC_DLR , KC_AMPR,     KC_ASTR, KC_LABK, KC_RABK, KC_LBRC, XXXXXXX, XXXXXXX,
-        KC_DEL , KC_CIRC, KC_W   , KC_F   , KC_P   , KC_B   ,     KC_J   , KC_L   , KC_U   , KC_Y   , KC_RBRC, INS_SHT,
-        KC_TAB , KC_Q   , KC_R   , KC_S   , KC_T   , KC_G   ,     KC_M   , KC_N   , KC_E   , KC_I   , KC_SLSH, KC_BSLS,
-        GUI_ESC, KC_A   , KC_X   , KC_C   , KC_D   , KC_V   ,     KC_K   , KC_H   , KC_COMM, KC_DOT , KC_O   , KC_GRV ,
-        KC_LSFT, KC_Z   , XXXXXXX, XXXXXXX, XXXXXXX, MK_BSPC,     FN_ENT , XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT, CAPSWRD,
-        KC_BSPC, CPY_PST, ALT_LYS, KC_LCTL, OS_RSFT, XXXXXXX,     XXXXXXX, FN_SPC , FN_MINS, FN_EQL , XXXXXXX, XXXXXXX 
+        KC_DEL , KC_CIRC, KC_AT  , KC_HASH, KC_DLR , KC_AMPR,     KC_ASTR, KC_LABK, KC_RABK, KC_LBRC, KC_RBRC, INS_SHT,
+        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SLSH, KC_BSLS,
+        GUI_ESC, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   ,     KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_GRV ,
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,     KC_K   , KC_H   , KC_COMM, KC_DOT , KC_QUOT, CAPSWRD,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, CPY_PST, ALT_LYS, KC_LCTL, OS_RSFT, KC_BSPC,     FN_ENT , FN_SPC , FN_MINS, FN_EQL , ALT_TAB, XXXXXXX 
     ),
     [_QW] = LAYOUT(
         _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
@@ -51,34 +51,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_MK] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, TG(_MK), PRV_WIN, NEW_TAB, NXT_WIN, XXXXXXX,     DM_REC2, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U, XXXXXXX,
+        XXXXXXX, XXXXXXX, PRV_TAB, ALT_TAB, NXT_TAB, XXXXXXX,     DM_REC1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, XXXXXXX,
+        XXXXXXX, XXXXXXX, PRV_PG , CLS_TAB, NXT_PG , XXXXXXX,     DM_RSTP, DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN4, KC_BTN5, XXXXXXX
     ),
     [_NP] = LAYOUT(
         _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_7   , KC_8   , KC_9   , KC_PAST,     KC_PAST, KC_7   , KC_8   , KC_9   , _______, _______,
-        KC_TAB , KC_PPLS, KC_4   , KC_5   , KC_6   , KC_PSLS,     KC_PSLS, KC_4   , KC_5   , KC_6   , KC_PPLS, KC_TAB ,
-        KC_ESC , KC_PMNS, KC_1   , KC_2   , KC_3   , KC_PERC,     KC_PERC, KC_1   , KC_2   , KC_3   , KC_PMNS, KC_ESC ,
-        KC_COMM, KC_EQL , _______, _______, _______, _______,     _______, _______, _______, _______, KC_EQL , KC_COMM,
-        _______, _______, KC_DOT , KC_0   , KC_PENT, _______,     _______, KC_0   , KC_0   , KC_DOT , _______, _______
+        KC_TAB , KC_PPLS, KC_7   , KC_8   , KC_9   , KC_PAST,     KC_PAST, KC_7   , KC_8   , KC_9   , KC_PPLS, KC_TAB ,
+        KC_ESC , KC_PMNS, KC_4   , KC_5   , KC_6   , KC_PSLS,     KC_PSLS, KC_4   , KC_5   , KC_6   , KC_PMNS, KC_ESC ,
+        KC_COMM, KC_EQL , KC_1   , KC_2   , KC_3   , KC_PERC,     KC_PERC, KC_1   , KC_2   , KC_3   , KC_EQL , KC_COMM,
+        _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_DOT , KC_0   , KC_PENT, _______,     _______, FN_SPC , KC_0   , KC_DOT , _______, _______
     ),
     [_FN] = LAYOUT(
-        _______, _______, KC_F3  , KC_F4  , KC_F5  , KC_F6  ,     KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, _______,
-        KC_F1  , KC_F2  , KC_7   , KC_8   , KC_9   , KC_PAST,     KC_BRIU, KC_HOME, KC_UP  , KC_END , KC_F11 , KC_F12 ,
-        KC_TAB , KC_PPLS, KC_4   , KC_5   , KC_6   , KC_PSLS,     KC_BRID, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP, KC_BSPC,
-        KC_ESC , KC_PMNS, KC_1   , KC_2   , KC_3   , KC_PERC,     KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_PGDN, KC_DEL ,
-        KC_COMM, KC_EQL , _______, _______, _______, _______,     _______, _______, _______, _______, KC_MPLY, KC_MNXT,
+        KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,     KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+        KC_TAB , KC_PPLS, KC_7   , KC_8   , KC_9   , KC_PAST,     KC_BRIU, KC_HOME, KC_UP  , KC_END , KC_PGUP, KC_BSPC,
+        KC_ESC , KC_PMNS, KC_4   , KC_5   , KC_6   , KC_PSLS,     KC_BRID, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_DEL ,
+        KC_COMM, KC_EQL , KC_1   , KC_2   , KC_3   , KC_PERC,     KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT,
+        _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
         _______, _______, KC_DOT , KC_0   , KC_PENT, _______,     _______, TO(_BS), KC_RCTL, KC_RALT, _______, _______
     ),
     [CMD] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, CG_NORM, TG(_FN), KC_PSCR, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, TG(_QW),  RESET , KC_SLCK, CG_TOGG, TG(_QW),     CG_SWAP, KC_NLCK, EEP_RST, EEP_RST, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, XXXXXXX,
+        XXXXXXX, TG(_QW), CG_NORM, TG(_FN), KC_PSCR, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,  RESET , KC_SLCK, CG_TOGG, TG(_QW),     CG_SWAP, KC_NLCK, EEP_RST, EEP_RST, CG_TOGG, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
